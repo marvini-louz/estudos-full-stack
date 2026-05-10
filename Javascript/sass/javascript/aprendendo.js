9db4c0e839981a907fdddd241a319fdc80f0ddf2
Marcelo <mvinicius.louza@gmail.com>
2026-05-10 04:07:44 -0300

    Adicionado mais exercícios da aula

--- Javascript/sass/javascript/aprendendo.js
+++ Javascript/sass/javascript/aprendendo.js
@@ -11,3 +11,43 @@ var identidade = {
 
 const investiomentoMensal = [1200, 2340, 2340, 1249, 2930, 2340];
 console.log("Segundo index é: " + investiomentoMensal[1]);
+
+// Escopos
+
+const primeiraVariavelDoCodigo = "texto inicial";
+
+function printToConsole() {
+    const primeiraVariavelDoCodigo = "segundo texto";
+
+    function secondFunction() {
+        const primeiraVariavelDoCodigo = "terceiro texto";
+        console.log("imprimindo segunda mensagem");
+    }
+
+    console.log(primeiraVariavelDoCodigo);
+    secondFunction();
+}
+
+printToConsole();
+
+// Métodos
+
+let casa = {
+    cor: "white",
+
+    sala: 15,
+    cozinha: 10,
+    banheiro: 8,
+    quarto: 20,
+
+    calcularTamanhoTotalCasaEmM2: function () {
+        const tamanhoTotal =
+            this.sala + this.cozinha + this.banheiro + this.quarto;
+
+        console.log(`o tamanho da casa é de ${tamanhoTotal}m²`);
+
+        return tamanhoTotal;
+    },
+};
+
+casa.calcularTamanhoTotalCasaEmM2();
--- Javascript/sass/javascript/aulas.js
+++ Javascript/sass/javascript/aulas.js
@@ -31,3 +31,95 @@ console.log("Operação 3 OR: " + (bool1 || bool2 || bool3));
 
 // ===== Exercício 3
 console.log("\n// ===== Exercício 3\n");
+
+const arrayNums = [1, 2, 3, 4, 5];
+console.log(arrayNums[0]);
+console.log("O valor 0 da lista é: " + arrayNums[0]);
+
+// ===== Exercício 4
+console.log("\n// ===== Exercício 4\n");
+
+const arrayNums2 = [1, 2, 3, 4, 5];
+console.log(arrayNums2[0]);
+
+// ===== Exercício 5
+console.log("\n// ===== Exercício 5\n");
+
+function squareRoot(numBase) {
+    return numBase ** (1 / 2);
+}
+
+function bhaskara(numA, numB, numC) {
+    let delta = numB ** 2 - 4 * numA * numC;
+    if (delta < 0) {
+        return console.log("Delta negativo - tente outros valores");
+    }
+
+    let x1 = (-numB + squareRoot(delta)) / (2 * numA);
+    let x2 = (-numB - squareRoot(delta)) / (2 * numA);
+
+    console.log("X1 linha: " + x1.toFixed(2));
+    console.log("X2 linha: " + x2.toFixed(2));
+}
+
+bhaskara(5, 4, -3);
+
+// ===== Exercício 6
+console.log("\n// ===== Exercício 6\n");
+
+function mediaProvas(notas) {
+    let total = 0;
+    for (var i = 0; i < notas.length; i++) {
+        total += notas[i];
+    }
+    var media = total / notas.length;
+
+    return media;
+}
+
+console.log("A media é: " + mediaProvas([30, 30, 30, 30]));
+
+// ===== Exercício 7
+console.log("\n// ===== Exercício 7\n");
+
+function mediaProvas2(notas, provaFinal, mediaReq) {
+    let total = 0;
+    const mediaRequerida = mediaReq;
+    for (var i = 0; i < notas.length; i++) {
+        total += notas[i];
+    }
+    let media = (total + provaFinal) / (notas.length + 1);
+
+    if (media < mediaRequerida) {
+        console.log("Não atingiu a média de " + mediaRequerida);
+    } else {
+        console.log("Média atingida com sucesso");
+    }
+
+    return media.toFixed(2);
+}
+
+console.log("A media é: " + mediaProvas2([40, 50, 40], 50, 60));
+
+// ===== Exercício 8
+console.log("\n// ===== Exercício 8\n");
+
+function celsiusToFarenheit(graus) {
+    farenheit = (9 / 5) * graus + 32;
+
+    return console.log(graus + "°C em Farenheit é: " + farenheit);
+}
+
+celsiusToFarenheit(22);
+
+// ===== Exercício 9
+console.log("\n// ===== Exercício 9\n");
+
+function valorAtualizado(valor) {
+    const imposto = 0.08875;
+    var valorComImposto = valor * (1 + imposto);
+
+    return valorComImposto.toFixed(2);
+}
+
+console.log("Valor atualizado é: " + valorAtualizado(50));
