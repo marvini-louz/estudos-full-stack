798d756e3d24021984bafdab82d9324659017e57
Marcelo <mvinicius.louza@gmail.com>
2026-05-20 10:40:43 -0300

    adding learning of async/await of promises

--- Javascript/scripts/aprendendo.js
+++ Javascript/scripts/aprendendo.js
@@ -86,3 +86,58 @@ const alunosAprovadosNome = alunosAprovados.map(function (aluno) {
     return aluno.nome;
 });
 console.log(alunosAprovadosNome);
+
+//promise
+// As Promises são uma maneira muito melhor de trabalhar com código assíncrono
+// em JavaScript do que a antiga abordagem de callbacks, propensa a erros.
+function getUser() {
+    console.log("Recuperando informações do usuário");
+
+    const promise1 = new Promise((resolve) => {
+        setTimeout(() => {
+            resolve({ id: 0, name: "Daniel", surname: "Porto" });
+        }, 2000);
+    });
+
+    return promise1;
+}
+
+function getFriendList(user) {
+    console.log(
+        `Recuperando lista de amigos do usuário ${user.name} ${user.surname}`,
+    );
+
+    const promise2 = new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(["joão", "bruna", "felipe", "Lira"]);
+        }, 2000);
+    });
+
+    return promise2;
+}
+
+function getPhotos(username) {
+    console.log(`Recuperando fotos do usuário: ${username}`);
+    const promise3 = new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(["egito", "bahamas", "maldivas", "suécia"]);
+        }, 2000);
+    });
+    return promise3;
+}
+
+getUser()
+    .then((user) => getFriendList(user))
+    .then((friends) => getPhotos(friends[3]))
+    .then((photos) => console.log(photos));
+
+//Async/Await
+
+async function displayPhotos() {
+    const user = await getUser();
+    const friends = await getFriendList(user);
+    const photos = await getPhotos(friends[3]);
+    console.log(photos);
+}
+
+displayPhotos();
