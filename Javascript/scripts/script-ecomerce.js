a7b1b66a021cf44db24a9fc37e0bc6ae4d6e6efd
Marcelo <mvinicius.louza@gmail.com>
2026-05-26 13:56:27 -0300

    implements of projeto 3 - e-Commerce

---
+++ Javascript/scripts/script-ecomerce.js
@@ -0,0 +1,11 @@
+const catalogo = [];
+
+for (const indexProduct of catalogo) {
+    const productBox = `
+    <div>
+
+    </div>
+    `;
+
+    document.getElementById("card-product-box").innerHTML(productBox);
+}
